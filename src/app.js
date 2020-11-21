0398bb1c92a1fc6990f623079ce7d65b2542fb4f
Wadaarik <nicolasboudier12@gmail.com>
2020-11-21 22:39:32 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+   parent: document.querySelector('.distortion'),//l'endroit où l'on place l'image
+   intensity: 0.2,
+   image1: './houna_rahn_homepage.jpg',
+   image2: './houna_rahn_first_illustration_tall.jpg',
+   angle: Math.PI / 8,
+   displacementImage: './heightMap.png'
+});
